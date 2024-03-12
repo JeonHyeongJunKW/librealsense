247ab91847a21cad2f9845cc55ea2df3aca5aa4f
JeonHyeongJunKW <jhj@robotis.com>
2024-03-12 17:08:50 +0900

    Used null pointer
    Signed-off-by: JeonHyeongJunKW <jhj@robotis.com>

--- src/proc/cuda/cuda-align.cuh
+++ src/proc/cuda/cuda-align.cuh
@@ -10,10 +10,7 @@ namespace librealsense
     class align_cuda_helper
     {
     public:
-        align_cuda_helper() :
-            _d_depth_in(nullptr),
-            _d_other_in(nullptr),
-            _d_aligned_out(nullptr) {}
+        align_cuda_helper() {}
         ~align_cuda_helper();
         void align_other_to_depth(unsigned char* h_aligned_out, const uint16_t* h_depth_in,
             float depth_scale, const rs2_intrinsics& h_depth_intrin, const rs2_extrinsics& h_depth_to_other,
@@ -24,14 +21,14 @@ namespace librealsense
             const rs2_intrinsics& h_other_intrin);
 
     private:
-        uint16_t *       _d_depth_in;
-        unsigned char *  _d_other_in;
-        unsigned char *  _d_aligned_out;
-        int2 *           _d_pixel_map;
+        uint16_t *       _d_depth_in = nullptr;
+        unsigned char *  _d_other_in = nullptr;
+        unsigned char *  _d_aligned_out = nullptr;
+        int2 *           _d_pixel_map = nullptr;
 
-        rs2_intrinsics * _d_other_intrinsics;
-        rs2_intrinsics * _d_depth_intrinsics;
-        rs2_extrinsics * _d_depth_other_extrinsics;
+        rs2_intrinsics * _d_other_intrinsics = nullptr;
+        rs2_intrinsics * _d_depth_intrinsics = nullptr;
+        rs2_extrinsics * _d_depth_other_extrinsics = nullptr;
     };
 }
 #endif // RS2_USE_CUDA
